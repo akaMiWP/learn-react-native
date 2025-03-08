7f8832ca1155717f55f1b2fca83be905a9a50033
akaMiWP <bangkokappdevelopment@gmail.com>
2025-03-08 23:36:49 +0700

    Rendered an image on index page

--- lesson02/app/(tabs)/index.tsx
+++ lesson02/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import React from "react";
+import image from "@/assets/images/iced-coffee.png";
 
 const app = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>App</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>App</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -16,10 +19,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   text: {
     color: "white",
     fontSize: 42,
     fontWeight: "bold",
     textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
 });
