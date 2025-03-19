01159ed2132466b7f41d11a4b2ca1eb93c717c9b
akaMiWP <bangkokappdevelopment@gmail.com>
2025-03-19 21:54:10 +0700

    Added a useFetch hook

---
+++ movie-app/services/useFetch.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+
+      const result = await fetchFunction();
+      setData(result);
+    } catch (error) {
+      setError(error instanceof Error ? error : new Error("An error occured"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
