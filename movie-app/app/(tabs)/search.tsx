57f48de9875af0edfdcb91a77dd6bb5c41fb86d3
akaMiWP <bangkokappdevelopment@gmail.com>
2025-03-23 00:35:30 +0700

    Painted Search page bg

--- movie-app/app/(tabs)/search.tsx
+++ movie-app/app/(tabs)/search.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        className="flex-1 absolute z-0"
+        source={images.bg}
+        resizeMode="cover"
+      />
     </View>
   );
 };
