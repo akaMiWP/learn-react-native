e067552cb9fafd77c8fb20230d2a0baf83ea49bc
akaMiWP <bangkokappdevelopment@gmail.com>
2025-03-23 01:10:34 +0700

    Added functionalities for Search screen

--- movie-app/app/(tabs)/search.tsx
+++ movie-app/app/(tabs)/search.tsx
@@ -1,14 +1,90 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
 import { images } from "@/constants/images";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const {
+    data,
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const func = async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    };
+
+    func();
+  }, [searchQuery]);
+
   return (
     <View className="flex-1 bg-primary">
       <Image
         className="flex-1 absolute z-0"
         source={images.bg}
         resizeMode="cover"
+      />
+
+      <FlatList
+        className="px-5"
+        data={data}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image className="w-12 h-10" source={icons.logo} />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies ..."
+                value={searchQuery}
+                onChangeText={(text) => setSearchQuery(text)}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                className="my-3"
+                size="large"
+                color="#0000ff"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading && !error && searchQuery.trim() && data?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search Results for {` `}
+                <Text className="text-accent">{searchQuery}</Text>
+              </Text>
+            )}
+          </>
+        }
       />
     </View>
   );
